147ae48e448fca9c8c67320c8794dd42ae4764c0
Albert I <kras@raphielgang.org>
2023-05-26 16:19:59 +0530

    spes: Inherit several Android Go configurations Beneficial for everyone, even on non-Go devices.
    Signed off by : Albert I <kras@raphielgang.org>

--- device.mk
+++ device.mk
@@ -118,6 +118,11 @@ PRODUCT_COPY_FILES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += $(LOCAL_PATH)
 
+# Speed profile services and wifi-service to reduce RAM and storage
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
+
 # Thermal
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine-dubai.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-dubai.conf
